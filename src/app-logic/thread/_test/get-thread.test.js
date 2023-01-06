405b4c5af3f5fb85096f9c82808ccbbb8ef2e6cb
boedegoat <bhremada.fka@gmail.com>
2023-01-06 18:58:26 +0700

    make 100% test coverage

--- src/app-logic/thread/_test/get-thread.test.js
+++ src/app-logic/thread/_test/get-thread.test.js
@@ -34,6 +34,81 @@ describe("getThread", () => {
         );
     });
 
+    it("gets thread correctly that include all thread comments", async () => {
+        const payload = {
+            threadId: "thread-1",
+        };
+
+        const mockThreadDB = {};
+        const mockCommentDB = {};
+        const mockReplyDB = {};
+
+        mockThreadDB.checkIsThreadExistById = jest
+            .fn()
+            .mockImplementation(() => Promise.resolve());
+
+        mockThreadDB.getThreadById = jest.fn().mockImplementation(() =>
+            Promise.resolve({
+                id: "thread-1",
+                title: "sebuah thread",
+                body: "sebuah body thread",
+                date: "2021-08-08T07:19:09.775Z",
+                username: "dicoding",
+            })
+        );
+
+        mockCommentDB.getCommentsByThreadId = jest.fn().mockImplementation(() =>
+            Promise.resolve([
+                {
+                    id: "comment-1",
+                    username: "johndoe",
+                    date: "2021-08-08T07:22:33.555Z",
+                    content: "comment",
+                    is_deleted: false,
+                },
+            ])
+        );
+
+        mockReplyDB.getRepliesByCommentId = jest
+            .fn()
+            .mockImplementation(() => []);
+
+        const expectedReturnedThread = {
+            id: "thread-1",
+            title: "sebuah thread",
+            body: "sebuah body thread",
+            date: "2021-08-08T07:19:09.775Z",
+            username: "dicoding",
+            comments: [
+                {
+                    id: "comment-1",
+                    username: "johndoe",
+                    date: "2021-08-08T07:22:33.555Z",
+                    content: "comment",
+                },
+            ],
+        };
+
+        const getThread = buildGetThread({
+            validatePayload,
+            threadDB: mockThreadDB,
+            commentDB: mockCommentDB,
+            replyDB: mockReplyDB,
+        });
+
+        const actualReturnedThread = await getThread(payload);
+
+        expect(actualReturnedThread).toEqual(expectedReturnedThread);
+        expect(mockThreadDB.checkIsThreadExistById).toBeCalledWith(
+            payload.threadId
+        );
+        expect(mockThreadDB.getThreadById).toBeCalledWith(payload.threadId);
+        expect(mockCommentDB.getCommentsByThreadId).toBeCalledWith(
+            payload.threadId
+        );
+        expect(mockReplyDB.getRepliesByCommentId).toBeCalledWith("comment-1");
+    });
+
     it("gets thread correctly that include all thread comments and replies", async () => {
         const payload = {
             threadId: "thread-1",
@@ -64,6 +139,7 @@ describe("getThread", () => {
                     username: "johndoe",
                     date: "2021-08-08T07:22:33.555Z",
                     content: "sebuah comment",
+                    is_deleted: true,
                 },
             ])
         );
@@ -74,12 +150,14 @@ describe("getThread", () => {
                 content: "sebuah balasan",
                 date: "2021-08-08T07:59:48.766Z",
                 username: "johndoe",
+                is_deleted: false,
             },
             {
                 id: "reply-2",
                 content: "sebuah balasan",
                 date: "2021-08-08T08:07:01.522Z",
                 username: "dicoding",
+                is_deleted: true,
             },
         ]);
 
@@ -94,7 +172,7 @@ describe("getThread", () => {
                     id: "comment-1",
                     username: "johndoe",
                     date: "2021-08-08T07:22:33.555Z",
-                    content: "sebuah comment",
+                    content: "**komentar telah dihapus**",
                     replies: [
                         {
                             id: "reply-1",
@@ -104,7 +182,7 @@ describe("getThread", () => {
                         },
                         {
                             id: "reply-2",
-                            content: "sebuah balasan",
+                            content: "**balasan telah dihapus**",
                             date: "2021-08-08T08:07:01.522Z",
                             username: "dicoding",
                         },
