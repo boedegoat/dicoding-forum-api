1a3bf52f911b5bb50d687e8e5a66a90304001c1f
boedegoat <bhremada.fka@gmail.com>
2023-01-14 10:10:20 +0700

    create GET /user-agent

--- src/interfaces/webserver/hapi-server.js
+++ src/interfaces/webserver/hapi-server.js
@@ -57,6 +57,13 @@ const buildHapiServer = async ({
         },
         {
             method: "GET",
+            path: "/user-agent",
+            handler: (request) => ({
+                value: request.headers["user-agent"],
+            }),
+        },
+        {
+            method: "GET",
             path: "/500-test",
             handler: () => {
                 // eslint-disable-next-line no-undef
